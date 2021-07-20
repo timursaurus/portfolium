4d52d367c7164b5cbf335ad74749c6608df297cc
timursaurus <bolotovtmr@gmail.com>
2021-07-20 16:58:42 +0600

    pages, pages and pages!

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,19 @@
 import WindiCSS from 'vite-plugin-windicss'
+const { resolve } = require('path')
 
 export default {
   plugins: [
     WindiCSS(),
   ],
 }
+
+// module.exports = {
+//   build: {
+//     rollupOptions: {
+//       input: {
+//         main: resolve(__dirname, 'index.html'),
+//         education: resolve(__dirname, 'education/index.html')
+//       }
+//     }
+//   }
+// }
